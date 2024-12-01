05fda9281999755afcfd324ba83b42dec7d2b48c
HenleyHwang <hwangtianyu@hotmail.com>
2024-12-01 17:22:21 -0500

    Update kernels.py

--- BCNN-SCP/src/kernels.py
+++ BCNN-SCP/src/kernels.py
@@ -5,7 +5,7 @@ import torch
 
 class SpatialCovariance(ABC):
     def __call__(self, xs, ys):
-        points = [torch.tensor([x, y], dtype=torch.float) for x in xs for y in ys]
+        points = torch.cartesian_prod(xs, ys)
         covar = torch.zeros(len(points), len(points))
         for i in range(len(points)):
             for j in range(i, len(points)):
@@ -26,27 +26,26 @@ class RBFCovariance(SpatialCovariance):
         self.l = l
 
     def kernel(self, x, y):
-        diff = torch.norm(x - y)
-        return self.a * torch.exp(-(diff**2) / (2 * self.l**2))
+        dist = torch.linalg.vector_norm(x - y)
+        return self.a * torch.exp(-(dist**2) / (2 * self.l**2))
 
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
     # Visualize RBF kernel
-    x = torch.tensor([0])
-    ys = torch.linspace(-5, 5, 100)
-    plt.plot(ys, [RBFCovariance(1, 1).kernel(x, y) for y in ys], label="RBF(a=1, l=1)")
-    plt.plot(ys, [RBFCovariance(1, 2).kernel(x, y) for y in ys], label="RBF(a=1, l=2)")
-    plt.plot(ys, [RBFCovariance(2, 1).kernel(x, y) for y in ys], label="RBF(a=2, l=1)")
+    xs = torch.linspace(-5, 5, 100)
+    for a, l in [(1, 1), (1, 2), (2, 1)]:
+        rbf = RBFCovariance(a, l)
+        plt.plot(xs, [rbf.kernel(x, 0.0) for x in xs], label=f"RBF(a={a}, l={l})")
     plt.xlabel("|x - y|")
     plt.legend()
     plt.show()
 
     # Visualize RBF covariance matrix
     rbf = RBFCovariance(1, 1)
-    xs = torch.tensor([0, 1, 2])
-    ys = torch.tensor([0, 1, 2])
+    xs = torch.tensor([0.0, 1.0, 2.0])
+    ys = torch.tensor([0.0, 1.0, 2.0])
     covar = rbf(xs, ys)
     plt.imshow(covar)
     plt.show()
